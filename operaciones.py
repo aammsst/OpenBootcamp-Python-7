1415cd4c7bfcc764ac8adfb0a25f399fcf949e24
Fabricio Navarro <Navarro.fa998@gmail.com>
2022-07-06 19:25:17 -0300

    creación del módulo - ejercicio 1

---
+++ operaciones.py
@@ -0,0 +1,23 @@
+def suma(a=0, b=0):
+    """
+    Suma a + b
+    """
+    return a + b
+
+def resta(a=0, b=0):
+    """
+    Resta a - b
+    """
+    return a - b
+
+def multiplica(a=0, b=0):
+    """
+    Multiplica a * b
+    """
+    return a * b
+
+def divide(a=0, b=1):
+    """
+    Divide a / b
+    """
+    return a / b
